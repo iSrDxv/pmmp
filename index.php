c12a6b626ed35b445b38890c1b88e53cf217b2a3
Claudio T <63529134+iSrDxv@users.noreply.github.com>
2022-02-13 23:47:59 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
+<?php
+//$http_client_ip = $_SERVER["HTTP_CLIENT_IP"];
+$address = $_SERVER["REMOTE_ADDR"];
 
+//echo $http_client_ip;
+echo PHP_EOL;
+echo $address;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <title>Inicio</title>
+  
+</head>
+<body>
+</body>
+</html>
